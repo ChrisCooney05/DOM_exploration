b517350adf311d81ce9652d0d132b52fff9be189
Chris Cooney <chris.cooney05@hotmail.co.uk>
2020-07-14 08:41:13 +0100

    able to toggle done class off and on with click

--- script.js
+++ script.js
@@ -3,28 +3,35 @@ var input = document.getElementById("userinput");
 var ul = document.querySelector("ul");
 
 function inputLength() {
-	return input.value.length;
+  return input.value.length;
 }
 
 function createListElement() {
-	var li = document.createElement("li");
-	li.appendChild(document.createTextNode(input.value));
-	ul.appendChild(li);
-	input.value = "";
+  var li = document.createElement("li");
+  li.appendChild(document.createTextNode(input.value));
+  ul.appendChild(li);
+  input.value = "";
 }
 
 function addListAfterClick() {
-	if (inputLength() > 0) {
-		createListElement();
-	}
+  if (inputLength() > 0) {
+    createListElement();
+  }
 }
 
 function addListAfterKeypress(event) {
-	if (inputLength() > 0 && event.keyCode === 13) {
-		createListElement();
-	}
+  if (inputLength() > 0 && event.keyCode === 13) {
+    createListElement();
+  }
+}
+
+function toggleDone(event) {
+  let li = event.target;
+  li.classList.toggle("done");
 }
 
 button.addEventListener("click", addListAfterClick);
 
 input.addEventListener("keypress", addListAfterKeypress);
+
+ul.addEventListener("click", toggleDone);
