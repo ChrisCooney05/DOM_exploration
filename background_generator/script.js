88750dedd68e153b8892ed08eff3567feae12d58
Chris Cooney <chris.cooney05@hotmail.co.uk>
2020-07-16 09:17:33 +0100

    finished background colour generator

--- background_generator/script.js
+++ background_generator/script.js
@@ -2,6 +2,7 @@ const css = document.querySelector("h3");
 const colour1 = document.querySelector(".color1");
 const colour2 = document.querySelector(".color2");
 const body = document.getElementById("gradient");
+const button = document.querySelector(".btn");
 
 function setGradient() {
   body.style.background =
@@ -15,8 +16,26 @@ function addTextToPage(style) {
   css.textContent = `${style};`;
 }
 
+function addRandomColour() {
+  colour1.value = generateRandomColour();
+  colour2.value = generateRandomColour();
+  setGradient();
+}
+
+function generateRandomColour() {
+  let characters = "0123456789ABCDEF";
+  let colour = "#";
+
+  for (var i = 0; i < 6; i++) {
+    colour += characters[Math.floor(Math.random() * 16)];
+  }
+
+  return colour;
+}
 colour1.addEventListener("input", setGradient);
 
 colour2.addEventListener("input", setGradient);
+
+button.addEventListener("click", addRandomColour);
 
 setGradient();
