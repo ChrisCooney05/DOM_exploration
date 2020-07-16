c4e9e6cab8b278dbbfab1ed885037675a5309ff2
Chris Cooney <chris.cooney05@hotmail.co.uk>
2020-07-16 07:15:17 +0100

    added comments and completed

--- script.js
+++ script.js
@@ -8,17 +8,21 @@ function inputLength() {
 
 function createListElement() {
   var li = document.createElement("li");
-  li.appendChild(document.createTextNode(input.value));
+  li.appendChild(document.createTextNode(`${input.value} `));
   ul.appendChild(li);
   input.value = "";
+  //reset input box to an empty string
   createButton(li);
+  //start function that will add a button
 }
 
 function createButton(li) {
   let button = document.createElement("button");
   button.appendChild(document.createTextNode("Delete"));
   li.appendChild(button);
+  //append button to list item
   button.onclick = removeTarget;
+  //on button click, removeTarget function is called
 }
 
 function addListAfterClick() {
@@ -36,10 +40,12 @@ function addListAfterKeypress(event) {
 function toggleDone(event) {
   let li = event.target;
   li.classList.toggle("done");
+  //done class added when user clicks on a ul element
 }
 
 function removeTarget(event) {
   event.target.parentNode.remove();
+  //deletes the parent of the button (list item)
 }
 
 button.addEventListener("click", addListAfterClick);
@@ -47,6 +53,7 @@ button.addEventListener("click", addListAfterClick);
 input.addEventListener("keypress", addListAfterKeypress);
 
 ul.addEventListener("click", toggleDone);
+//event lister added to the UL
 
 //for delete item task - look at setting button class to the value as the li- may also need to look at setting class of each
 //li to the value of its self.
