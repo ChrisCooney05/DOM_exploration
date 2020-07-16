3bc99b214bcef2fc31aea0361447cb617a8dd9e1
Chris Cooney <chris.cooney05@hotmail.co.uk>
2020-07-16 08:12:16 +0100

    split text generation into seprate function

--- background_generator/script.js
+++ background_generator/script.js
@@ -1,17 +1,18 @@
-var css = document.querySelector("h3");
-var color1 = document.querySelector(".color1");
-var color2 = document.querySelector(".color2");
-var body = document.getElementById("gradient");
+const css = document.querySelector("h3");
+const color1 = document.querySelector(".color1");
+const color2 = document.querySelector(".color2");
+const body = document.getElementById("gradient");
 
 function setGradient() {
-	body.style.background = 
-	"linear-gradient(to right, " 
-	+ color1.value 
-	+ ", " 
-	+ color2.value 
-	+ ")";
+  body.style.background =
+    "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
 
-	css.textContent = body.style.background + ";";
+  addTextToPage(body.style.background);
+  //adds text to css(h3) showing current linear-gradient
+}
+
+function addTextToPage(style) {
+  css.textContent = `${style};`;
 }
 
 color1.addEventListener("input", setGradient);
